bdb3a0232b52ff02e0b5de4feb2495114b797bc9
Berkayacar07 <berkay-acrr@hotmail.com>
2022-05-21 16:44:28 +0300

    Payment and orders history added.

---
+++ app/src/main/java/com/mobil/fooddelivery/Customer/Order2.kt
@@ -0,0 +1,10 @@
+package com.mobil.fooddelivery.Customer
+
+import com.mobil.fooddelivery.Customer.FoodCart
+import java.util.ArrayList
+
+class Order2(
+    var address: String,
+    var totalPrice: String,
+    var status: String
+)
